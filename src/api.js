b69f5f5d5d82db39c5afb03a714cae898bfa9769
ravan1010 <ravanravana177@gmail.com>
2026-02-07 21:48:26 -0800

    main

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: 'https://kocart-cws7.onrender.com',
+  baseURL: 'https://kocartserver.onrender.com',
   // baseURL: 'http://localhost:5001',
   withCredentials: true,
 });
