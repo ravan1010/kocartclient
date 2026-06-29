ed7f19ba0f3352e507b82fbca7dca6f3543aaa67
ravan1010 <ravanravana177@gmail.com>
2026-06-29 10:27:57 +0530

    main

--- src/admincategory/adminlandmark/adminlandmarkcreate.jsx
+++ src/admincategory/adminlandmark/adminlandmarkcreate.jsx
@@ -2,8 +2,6 @@ import { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom';
 import api from '../../api';
 
-
-
 const CreatePost = () => {
 
   const [name, setname] = useState('');
