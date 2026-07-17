96457f804b4089895c4b11a4cd4ab9a1db727565
ravan1010 <ravanravana177@gmail.com>
2026-07-17 11:55:11 +0530

    main

--- src/admincategory/adminlandmark/adminorder.jsx
+++ src/admincategory/adminlandmark/adminorder.jsx
@@ -237,7 +237,7 @@ const Adminorder = () => {
             key={order._id}
             className="bg-white rounded-xl shadow p-4"
           >
-            <div className="flex flex-col items-center">
+            <div className="flex flex-col">
               <div>
                 <p className="font-semibold">Order ID</p>
                 <p className="text-gray-600 break-all">
@@ -590,7 +590,6 @@ const Adminorder = () => {
       )}
     </div>
   )}
-
 </div>
   )
 }
