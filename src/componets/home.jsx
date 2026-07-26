fccda5859caba3b410dc1006ef11caea5507964e
ravan1010 <ravanravana177@gmail.com>
2026-07-26 11:27:54 +0530

    main

--- src/componets/home.jsx
+++ src/componets/home.jsx
@@ -98,7 +98,7 @@ const Home = () => {
             <button
               onClick={changeLocation}
               disabled={loadingLoc}
-              className="bg-blue-600 hover:bg-blue-700 text-white px-4 py-2 rounded-lg disabled:opacity-50"
+              className="px-4 py-2 rounded-lg disabled:opacity-50 border"
             >
               {loadingLoc ? "Updating..." : "📍 Change Location"}
             </button>
