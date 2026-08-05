9b31866880a25817a3d8d880ea4f1a23c2c32deb
ravan1010 <ravanravana177@gmail.com>
2026-08-05 18:06:24 +0530

    main

--- src/componets/servicetypes.jsx
+++ src/componets/servicetypes.jsx
@@ -6,6 +6,7 @@ export default function ServerTypes() {
     const navigate = useNavigate();
 
   const [services, setServices] = useState([]);
+  const [category, setcategory] = useState([]);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
@@ -15,6 +16,7 @@ export default function ServerTypes() {
         const res = await api.get("/api/services", {});
 
         setServices(res.data.serviceTypes);
+        setcategory(res.data.category);
       } catch (err) {
         console.log(err);
       } finally {
@@ -28,6 +30,41 @@ export default function ServerTypes() {
 
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+
+      {category.includes("foodANDbeverages") && (
+        <button
+          onClick={() => navigate("/resturant")}
+          className="group bg-white border border-gray-200 rounded-2xl p-6  shadow-sm hover:shadow-xl hover:border-green-500 transition-all duration-300 text-left"
+        >
+          <div className="text-5xl mb-4"></div>
+
+          <h2 className="text-lg font-bold text-gray-800 group-hover:text-green-600">
+            foods
+          </h2>
+
+          <p className="text-sm text-gray-500 mt-2">
+            Fast delivery within 5 km.
+          </p>
+        </button>
+      )}
+
+      {category.includes("groceryFruitsANDvegetables") && (
+        <button
+          onClick={() => navigate("/mart")}
+          className="group bg-white border border-gray-200 rounded-2xl p-6  shadow-sm hover:shadow-xl hover:border-green-500 transition-all duration-300 text-left"
+        >
+          <div className="text-5xl mb-4"></div>
+
+          <h2 className="text-lg font-bold text-gray-800 group-hover:text-green-600">
+            grocery and etc..
+          </h2>
+
+          <p className="text-sm text-gray-500 mt-2">
+            Fast delivery within 5 km.
+          </p>
+        </button>
+      )}
+
       {services.includes("bike_parcel") && (
         <button
           onClick={() => navigate("/bike-parcel")}
@@ -77,7 +114,7 @@ export default function ServerTypes() {
         </button>
       )}
 
-      {services.length === 0 && (
+      {services.length === 0 && category.length === 0 && (
         <div className="col-span-full text-center py-10 text-gray-500">
           No services available nearby.
         </div>
