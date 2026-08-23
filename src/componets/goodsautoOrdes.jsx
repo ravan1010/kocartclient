09cf3523a3b8fa6209edccb820832e897ef7a502
ravan1010 <ravanravana177@gmail.com>
2026-08-23 17:38:55 +0530

    main

--- src/componets/goodsautoOrdes.jsx
+++ src/componets/goodsautoOrdes.jsx
@@ -365,7 +365,7 @@ const [ratingSubmitted, setRatingSubmitted] = useState(false);
                               </p>
 
                               <p className="mt-0.5 truncate font-bold text-gray-900">
-                                {item.driver?._id || "Goods Auto Driver"}
+                                {item.driver?.vehicalName || "Goods Auto Driver"}
                               </p>
 
                               {/* Rating */}
