e2ba703d445e44be547a7ed853ff4cad2962a14a
ravan1010 <ravanravana177@gmail.com>
2026-09-24 12:13:06 +0530

    main

--- src/App.jsx
+++ src/App.jsx
@@ -1,66 +1,161 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import Home from './componets/home';
+// Main pages
+import Home from "./componets/home";
+import Profile from "./componets/profile";
+import GoodsAuto from "./componets/goodsauto";
+import GoodsAutoOrders from "./componets/goodsautoOrdes";
+import AutoOrders from "./componets/autoOrder";
 
-//og
-import SignupOne from './signup/signup';
-//admin info
+// Signup / authentication
+import SignupOne from "./signup/signup";
+import Clientverify from "./signup/otpverify";
+import ProtectedClient from "./signup/auth/authroute";
 
-import Profile from './componets/profile';
+// Location
+import AppFullScreenLocationPicker from "./hooks/AppFullScreenLocationPicker";
 
-import ReturnRefundPolicy from './footer/Return_and_Refund_Policy';
-import PrivacyPolicy from './footer/PrivacyPolicy';
-import TermsConditions from './footer/TermsConditions';
-import ContactInformationPolicy from './footer/ContactInformationPolicy';
-
-
-import ProtectedClient from './signup/auth/authroute';
-
-
-import GoodsAuto from './componets/goodsauto';
-import GoodsAutoOrders from './componets/goodsautoOrdes';
-import AutoOrders from './componets/autoOrder';
-import AppFullScreenLocationPicker from './hooks/AppFullScreenLocationPicker';
-import Clientverify from './signup/otpverify';
+// Footer / policies
+import ReturnRefundPolicy from "./footer/Return_and_Refund_Policy";
+import PrivacyPolicy from "./footer/PrivacyPolicy";
+import TermsConditions from "./footer/TermsConditions";
+import ContactInformationPolicy from "./footer/ContactInformationPolicy";
 
 
 function App() {
-
   return (
-    <>
-      <Router>
-        <Routes>
-          <Route path="/return-refund-policy" element={<ReturnRefundPolicy />} />
-          <Route path="/privacy-policy" element={<PrivacyPolicy />} />
-          <Route path="/terms-conditions" element={<TermsConditions />} />
-          <Route path="/contact-information-policy" element={<ContactInformationPolicy />} />
-   <Route
-  path="/location-picker"
-  element={<AppFullScreenLocationPicker />}
-/>
-        //user email or number for signup
-          <Route path='/signup' element={<SignupOne />} />
-          <Route path='/client-auth-success' element={<Clientverify />} />
+    <Router>
+      <Routes>
 
-          <Route element={<ProtectedClient />}>  
+        {/* =====================================================
+            PUBLIC ROUTES
+        ===================================================== */}
 
-          <Route path='/' element={<Home />} />
+        {/* Policies */}
+        <Route
+          path="/return-refund-policy"
+          element={<ReturnRefundPolicy />}
+        />
 
-          <Route path='/goodsAuto/:type' element={ <GoodsAuto /> } />
-          <Route path='/goodsAuto/order/:orderId' element={< GoodsAutoOrders /> } />
+        <Route
+          path="/privacy-policy"
+          element={<PrivacyPolicy />}
+        />
+
+        <Route
+          path="/terms-conditions"
+          element={<TermsConditions />}
+        />
+
+        <Route
+          path="/contact-information-policy"
+          element={<ContactInformationPolicy />}
+        />
+
+        {/* Location Picker */}
+        <Route
+          path="/location-picker"
+          element={<AppFullScreenLocationPicker />}
+        />
+
+        {/* Signup */}
+        <Route
+          path="/signup"
+          element={<SignupOne />}
+        />
+
+        {/* Client Google / OTP authentication success */}
+        <Route
+          path="/client-auth-success"
+          element={<Clientverify />}
+        />
 
 
-          <Route path='/auto/all/orders' element={<AutoOrders /> } />
+        {/* =====================================================
+            PROTECTED CLIENT ROUTES
+        ===================================================== */}
 
-            <Route path='/profile' element={
-              <Profile />
-            } />
-          </Route>
+        <Route element={<ProtectedClient />}>
 
-        </Routes>
-      </Router>
-    </>
-  )
+          {/* Home */}
+          <Route
+            path="/"
+            element={<Home />}
+          />
+
+          {/* =================================================
+              GOODS AUTO
+          ================================================= */}
+
+          {/* 3 Wheel / 4 Wheel selection */}
+          <Route
+            path="/goodsauto/:type"
+            element={<GoodsAuto />}
+          />
+
+          {/* Goods Auto order */}
+          <Route
+            path="/goodsauto/order/:orderId"
+            element={<GoodsAutoOrders />}
+          />
+
+          {/* =================================================
+              AUTO ORDERS
+          ================================================= */}
+
+          <Route
+            path="/auto/all/orders"
+            element={<AutoOrders />}
+          />
+
+          {/* =================================================
+              PROFILE
+          ================================================= */}
+
+          <Route
+            path="/profile"
+            element={<Profile />}
+          />
+
+        </Route>
+
+        {/* =====================================================
+            404
+        ===================================================== */}
+
+        <Route
+          path="*"
+          element={
+            <div
+              style={{
+                minHeight: "100vh",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+                flexDirection: "column",
+                padding: "20px",
+                textAlign: "center",
+              }}
+            >
+              <h1 style={{ fontSize: "48px", margin: 0 }}>
+                404
+              </h1>
+
+              <p
+                style={{
+                  color: "#666",
+                  marginTop: "10px",
+                }}
+              >
+                Page not found
+              </p>
+            </div>
+          }
+        />
+
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
